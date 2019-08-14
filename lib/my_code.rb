08dfddea859cda9077796ededf8f28b2cf0b872f
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-14 19:27:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,8 +13,10 @@ map_to_no_change(source_array)
 new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * +1 ) 
-    i -= 1
+    new.push( source_array[i] ) 
+    i += 1
   end
   return new
 end
+
+
