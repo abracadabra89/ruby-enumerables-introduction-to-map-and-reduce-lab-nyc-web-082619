b4119973ef736eb642b619cefd65b9766f2e0a7e
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-14 19:40:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,7 +34,7 @@ end
  new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * i ) 
+    new.push( source_array[i] * source_array[i] ) 
     i += 1
   end
   return new
